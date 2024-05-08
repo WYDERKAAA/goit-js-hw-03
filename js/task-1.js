c79e6b44ff4cdcc7a4618c9c72dff0e2d0b6d1cd
goit-markup-hw-01 <157325688+WYDERKAAA@users.noreply.github.com>
2024-05-08 20:12:26 +0200

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,13 +1,1 @@
-function makeTransaction(quantity, pricePerDroid, customerCredits) {
-  const totalPrice = quantity * pricePerDroid;
-  if (totalPrice > customerCredits) {
-    return "Insufficient funds!";
-  } else {
-    return `You ordered ${quantity} droids worth ${totalPrice} credits`;
-  }
-}
-console.log(makeTransaction(5, 3000, 23000)); // zamówiłeś 5 droidów wartych 15000 kredytów!
-console.log(makeTransaction(3, 1000, 15000));
-console.log(makeTransaction(10, 5000, 8000)); // brak kasy
-console.log(makeTransaction(8, 2000, 10000));
-console.log(makeTransaction(10, 500, 5000));
+
